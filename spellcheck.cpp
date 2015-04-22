b4aa5260dc0ac21e4a46fcc35586ea3348beb978
Sam <vsamster95@gmail.com>
2015-04-22 13:00:21 +0530

    Removed unnecessary code and added comments

--- spellcheck.cpp
+++ spellcheck.cpp
@@ -14,17 +14,16 @@ using namespace std;
 
 int LevenshteinDistance(string s,int s_len, string t,int t_len)
 {
-  //std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-  //std::transform(t.begin(), t.end(), t.begin(), ::tolower);
+ 
   int d[s_len][t_len];
  
- for(int i=0;i<s_len;i++)
- {
+  for(int i=0;i<s_len;i++)
+  {
 	for(int j=0;j<t_len;j++)
 	{
 		d[i][j]=0;
 	}
-}
+  }
  
   // source prefixes can be transformed into empty string by
   // dropping all characters
@@ -42,14 +41,14 @@ int LevenshteinDistance(string s,int s_len, string t,int t_len)
       {
           if(s[i] == t[j])
             d[i][j] = d[i-1][j-1];              // no operation required
-          else
-	  {
-		int a=d[i-1][j] + 1;
-		int b=d[i][j-1] + 1;
-		int c=d[i-1][j-1] + 1;
-		d[i][j]= (a<b) ? ((a<c)? a: c) : ((b<c)? b: c);
- 	   }
-	}
+        else
+	    {
+		  int a=d[i-1][j] + 1;
+		  int b=d[i][j-1] + 1;
+		  int c=d[i-1][j-1] + 1;
+		  d[i][j]= (a<b) ? ((a<c)? a: c) : ((b<c)? b: c);
+ 	    }
+	  }
     }
   return d[s_len-1][t_len-1];
 }
@@ -79,7 +78,7 @@ class SpellCheck
 	{
 		return i;
 	}
-	
+	//Check whether the given word from the text file is present in the dictionary or not and return bool value
 	int Check_ifPresent(string word)
 	{
 		std::transform(word.begin(), word.end(), word.begin(), ::tolower);
@@ -88,6 +87,10 @@ class SpellCheck
 		else
 			return 0;
 	}
+
+	/*Uses an iterator i across the Dictionary vector and applies the Distance forumla between each word in the dictionary 
+	  and the word from the text file being checked. If the edit distance is found to be less than or equal to 1 the word 
+	  is considered a valid alternative and is displayed as such. */
 	void Check_eDistance(string word)
 	{
 		vector <string>::iterator i;
@@ -105,15 +108,22 @@ class SpellCheck
 	
 };
 
+/*Takes the name of the file to be checked from the user and opens an input stream fin for it. Each word is read from the
+  file and the Dictionary is checked to see if the word is present i.e a valid word or not. If found, the next word is read
+  and if not the word is passed to the Check_eDistance function to generate alternatives.
+*/
+
+
 int main()
 {
 	SpellCheck S;
 	string input_word;
+	int f=1;
 	cout<<"Enter a File name\n";
-	char fname[30];
+	string fname;
 	cin>>fname;
 	ifstream fin(fname);
-	int f=1;
+	
 	if(!fin)
 	{
 		cout<<"Unable to open file\n";
